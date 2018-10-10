91b71b74c07048617cec02d3b309c09acedd94ef
Nikolaj B Hemmeshøj <nbh@cphbusiness.dk>
2018-10-10 09:40:56 +0200

    String er lavet privat og der er lavet getter og sætter

--- src/com/company/Node.java
+++ src/com/company/Node.java
@@ -11,8 +11,16 @@ public class Node {
         Kontainer = kontainer;
     }
 
+    public String getKontainer() {
+        return Kontainer;
+    }
 
-    // todo 11) du skal flytte din konto og personklasse fra sidte projekt over i indeværende projekt. // opret klasserne og kopier indholdet over.
+    public void setKontainer(String kontainer) {
+        Kontainer = kontainer;
+    }
+
+
+    // todo 11) du skal flytte din konto og personklasse fra sidte projekt over i indeværende projekt og ændre containeren så den er af typen konto. // opret klasserne og kopier indholdet over.
     // todo 12) lav nu de nødvendige der skal til for at du kan håndtere dine kontoer i din egen liste (stor opgave !)
 
 }

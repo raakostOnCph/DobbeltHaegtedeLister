5d4b177dbe1c9864308cfd73ccf933138099d7ed
Nikolaj B Hemmeshøj <nbh@cphbusiness.dk>
2018-10-10 15:16:11 +0200

    nu med lidt flere tilføjelser

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,15 +8,6 @@ public class Main {
 
         Liste liste = new Liste();
 
-        liste.insertFromHead(new Node("1"));
-        liste.insertFromHead(new Node("2"));
-        liste.insertFromHead(new Node("3"));
-        liste.insertFromHead(new Node("4"));
-        liste.insertFromHead(new Node("5"));
-        liste.insertFromHead(new Node("6"));
-        liste.insertFromHead(new Node("7"));
-
-        liste.printFromHead();
 
 
     }

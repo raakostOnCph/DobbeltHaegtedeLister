21e8ae5421b26a13150a177a073665d4b93a9cd4
Nikolaj B Hemmeshøj <nbh@cphbusiness.dk>
2018-10-10 11:05:50 +0200

    nu med lidt flere tilføjelser

--- src/com/company/Liste.java
+++ src/com/company/Liste.java
@@ -2,17 +2,22 @@ package com.company;
 
 public class Liste {
 
-Node head = null;
+    Node head = null;
 
-Node tail = null;
+    Node tail = null;
 
 
+    }
 
-// todo 1) lav en funktion der kan indsætte en knude i starten af listen.
+// todo 1) lav en funktion der kan indsætte en knude i starten af listen. et kald kunne se sådan du liste.insertFromHead(new Node("Blondie"))
 
-// todo 2) lav en funktion der kan indsætte en knude en enden af listen.
+// todo 2) lav en funktion der kan indsætte en knude en enden af listen. så ovenfor
+
+// todo (1-2).1 lav lav funktionerne om så de tager en streng i stedet for en Node. et kald kunne da se sådan du list.insertFromhead("Blondie");
 
 // todo 3) lav en funktion der kan udskrive listen forfra dvs. indholdet i knuderne.
+
+// todo 3.1 lave begge udskrivfunktoner om så de returnere en streng til main, ikke udskriver fra klassen til terminalen.
 
 // todo 4) lav en funktion der kan udskrive listen bagfra.
 
@@ -27,8 +32,6 @@ Node tail = null;
 // todo 9) lav en funktion der kan slette en knude som ikke behøver at være den første eller sidste.
 
 // todo 10) lav en funktion der kan indsætte en knude efter en anden.
-
-
 
 
 }

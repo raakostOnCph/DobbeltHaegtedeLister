99cb8da030701cb77c0df5593df40e0d2ec96587
Nikolaj B Hemmeshøj <nbh@cphbusiness.dk>
2018-10-10 09:38:09 +0200

    node og liste klasserne er implementeret, men så heller ikke mere. Opgaverne er tilføjet og placeret i den rigtigt klasser.

---
+++ src/com/company/Liste.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Liste {
+
+Node head = null;
+
+Node tail = null;
+
+
+
+// todo 1) lav en funktion der kan indsætte en knude i starten af listen.
+
+// todo 2) lav en funktion der kan indsætte en knude en enden af listen.
+
+// todo 3) lav en funktion der kan udskrive listen forfra dvs. indholdet i knuderne.
+
+// todo 4) lav en funktion der kan udskrive listen bagfra.
+
+// todo 5) lav en funktion der kan fjerne den første knude i listen.
+
+// todo 6) lav en funktion der kan fjerne den sidste knude i listen.
+
+// todo 7) lav en funktion der kan finde en bestemt knude.
+
+// todo 8) lav en funktion der kan opdatere en bestemt knude.
+
+// todo 9) lav en funktion der kan slette en knude som ikke behøver at være den første eller sidste.
+
+// todo 10) lav en funktion der kan indsætte en knude efter en anden.
+
+
+
+
+}
---
+++ src/com/company/Node.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Node {
+
+    Node next = null;
+    Node previous = null;
+
+    String Kontainer;
+
+    public Node(String kontainer) {
+        Kontainer = kontainer;
+    }
+
+
+    // todo 11) du skal flytte din konto og personklasse fra sidte projekt over i indeværende projekt. // opret klasserne og kopier indholdet over.
+    // todo 12) lav nu de nødvendige der skal til for at du kan håndtere dine kontoer i din egen liste (stor opgave !)
+
+}
